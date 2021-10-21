c55ecdb11c70b6c188c845f6ce3e7ea8a5326be6
unknown <shankhisinha1005@gmail.com>
2021-10-21 11:20:13 +0800

    Completed plot 2

---
+++ plot2.R
@@ -0,0 +1,24 @@
+#sets working directory
+setwd("C:/Users/Shankhi/Documents/exploratorydataanalysis")
+
+#unzips the file
+unzip("exdata_data_household_power_consumption.zip")
+
+#reading in txt file as a table
+data <-  read.table("household_power_consumption.txt", skip = 1, sep =";")
+
+#set the names for columns
+names(data) <- c("Date", "Time", "Global_Active", "Global_Reactive", "Voltage", "Global_Intensity", "Sub1", "Sub2", "Sub3")
+
+#subset the data needed
+dat <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+#formatting data
+dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
+dat$Time <- strptime(dat$Time, format="%H:%M:%S")
+dat[1:1440,"Time"] <- format(dat[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+dat[1441:2880,"Time"] <- format(dat[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+#plotting the plot
+plot(dat$Time, as.numeric(as.character(dat$Global_Active)), type = "1", xlab = " " ylab = "Global Active Power (kilowatts)")
+title(main = "Global Active Power VS Time")
